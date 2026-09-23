a84272797757493808d71ec21c793f5bc1980e16
Abhinerd <notbindra@gmail.com>
2026-09-23 23:41:38 +0530

    added --follow-symlinks to eza-tree alias

--- bash/.config/bash/aliases.sh
+++ bash/.config/bash/aliases.sh
@@ -1,4 +1,4 @@
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-alias eza-tree='eza -A --icons=always --tree -I ".git"'
+alias eza-tree='eza -A --icons=always --tree --follow-symlinks -I ".git"'
 alias eza='eza -a -1 --icons=always'
